3f5bef5330c9f26f77b5dd75198de6ed6b637ff2
lpreaux <contact@lucaspreaux.fr>
2025-08-14 16:47:01 +0200

    fix analytics with netlify as a proxy

--- src/app/_providers/posthog-provider.tsx
+++ src/app/_providers/posthog-provider.tsx
@@ -17,9 +17,8 @@ const DynamicPostHogPageView = dynamicLoader(
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-      api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-      defaults: "2025-05-24",
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     });
   }, []);
