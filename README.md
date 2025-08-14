bd7b842d079f8e7da1aa1c1e45d4752bd71a424c
lpreaux <contact@lucaspreaux.fr>
2025-08-14 18:11:08 +0200

    fix files and folder order

--- README.md
+++ README.md
@@ -4,10 +4,11 @@
 
 - [x] Set up fatabase and data model
 - [x] Move folder open state to URL
-- [ ] Add layout
 - [x] Add auth
 - [x] Add file uploading
-- [ ] Add analytics
-- [ ] Add delete
+- [x] Add analytics
+- [x] Keep file and folder in order
+- [ ] Add layout
 - [ ] Add homepage + onboarding
+- [ ] Add delete
 - [ ] Find a better solution for root folder
--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,45 +1,46 @@
 import "server-only";
 
-import { db } from "~/server/db/index";
-import { type DBFileInsertType, files_table } from "~/server/db/schema";
-import { eq } from "drizzle-orm";
+import {db} from "~/server/db/index";
+import {type DBFileInsertType, files_table} from "~/server/db/schema";
+import {asc, desc, eq} from "drizzle-orm";
 
 export const QUERIES = {
-  getFolderContent: function (folderId: number) {
-    return db
-      .select()
-      .from(files_table)
-      .where((f) => eq(f.parentId, folderId));
-  },
+    getFolderContent: function (folderId: number) {
+        return db
+            .select()
+            .from(files_table)
+            .where((f) => eq(f.parentId, folderId))
+            .orderBy(desc(files_table.type), asc(files_table.name));
+    },
 
-  getAllParents: async function (folderId: number) {
-    const parents = [];
-    let currentId: number | null = folderId;
-    while (currentId !== null) {
-      const parent = await db
-        .select()
-        .from(files_table)
-        .where((f) => eq(f.id, currentId!));
-      if (!parent[0]) {
-        throw new Error("Parent not found!");
-      }
-      parents.unshift(parent[0]);
-      currentId = parent[0].parentId;
-    }
-    return parents;
-  },
+    getAllParents: async function (folderId: number) {
+        const parents = [];
+        let currentId: number | null = folderId;
+        while (currentId !== null) {
+            const parent = await db
+                .select()
+                .from(files_table)
+                .where((f) => eq(f.id, currentId!));
+            if (!parent[0]) {
+                throw new Error("Parent not found!");
+            }
+            parents.unshift(parent[0]);
+            currentId = parent[0].parentId;
+        }
+        return parents;
+    },
 
-  getFolderById: async function (folderId: number) {
-    const folder = await db
-      .select()
-      .from(files_table)
-      .where((f) => eq(f.id, folderId));
-    return folder[0];
-  },
+    getFolderById: async function (folderId: number) {
+        const folder = await db
+            .select()
+            .from(files_table)
+            .where((f) => eq(f.id, folderId));
+        return folder[0];
+    },
 };
 
 export const MUTATIONS = {
-  createFile: function (file: DBFileInsertType) {
-    return db.insert(files_table).values(file);
-  },
+    createFile: function (file: DBFileInsertType) {
+        return db.insert(files_table).values(file);
+    },
 };
